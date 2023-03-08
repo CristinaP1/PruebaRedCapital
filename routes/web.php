83421d43148722c49bbd97c7c38d701a145118c3
Cristina <cpq002@alumnos.ucn.cl>
2023-03-08 00:19:47 -0300

    Se agrega la ruta de cotizaciones

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CotizacionCController;
+use App\Http\Controllers\CotizacionDController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/cotizaciones', CotizacionDController::class);
 });
 
 require __DIR__.'/auth.php';
