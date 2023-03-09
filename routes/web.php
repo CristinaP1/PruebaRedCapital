6d4b7ac2372bf024d447cef5b05ca7688ec2b036
Cristina <cpq002@alumnos.ucn.cl>
2023-03-09 09:51:29 -0300

    Se agrega la ruta de usuario

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CotizacionCController;
 use App\Http\Controllers\CotizacionDController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -28,7 +29,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/cotizaciones', CotizacionDController::class);
+    Route::resource('/cotizaciones', CotizacionCController::class);
+    Route::resource('/usuarios', UserController::class);
+    
+
 });
 
 require __DIR__.'/auth.php';
