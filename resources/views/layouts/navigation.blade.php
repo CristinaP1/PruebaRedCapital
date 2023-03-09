ea2105c10b48fff55aaca85ac029a16eec8631d2
Cristina <cpq002@alumnos.ucn.cl>
2023-03-09 09:51:54 -0300

    Se agrega la ruta de la opción usuario al menú

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('cotizaciones.index')" :active="request()->routeIs('cotizaciones.index')">
+                    <x-nav-link :href="route('usuarios.index')" :active="request()->routeIs('usuarios.index')">
                         {{ __('Usuarios') }}
                     </x-nav-link>
                 </div>
@@ -87,7 +87,7 @@
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('us')">
                 {{ __('Usuarios') }}
             </x-responsive-nav-link>
         </div>
